b888a74cba65ccbb258024d2d536270ef9eff58c
Whuwhie <150569061+Whuwhie@users.noreply.github.com>
2026-05-19 21:08:44 +0800

    Fix some docker bug

--- app/config/db.php
+++ app/config/db.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'host' => getenv('MYSQL_HOST'),
-    'port' => getenv('MYSQL_PORT'),
-    'dbname' => getenv('MYSQL_DBNAME'),
-    'username' => getenv('MYSQL_USERNAME'),
-    'password' => getenv('MYSQL_PASSWORD')
+    'host' => getenv('MYSQL_HOST') ?: getenv('DB_HOST') ?: '127.0.0.1',
+    'port' => getenv('MYSQL_PORT') ?: '3306',
+    'dbname' => getenv('MYSQL_DBNAME') ?: 'jobseeker',
+    'username' => getenv('MYSQL_USERNAME') ?: 'root',
+    'password' => getenv('MYSQL_PASSWORD') ?: ''
 ];
 
 ?>
